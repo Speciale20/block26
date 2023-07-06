666088338214e6af69089ed6396759420a4c094f
Sam <samspeciale1997@gmail.com>
2023-07-05 20:42:07 -0500

    Setting up the Table

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import ContactList from "./components/ContactList";
+("./components/ContactList.jsx");
+
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
