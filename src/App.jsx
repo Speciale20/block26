dc6439d13776314d817d41f404d3dbce4c9a7c25
Sam <samspeciale1997@gmail.com>
2023-08-22 12:17:31 -0500

    commented out CotactList import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import ContactList from "./components/ContactList";
+// import ContactList from "./components/ContactList";
 import SelectedContact from "./components/SelectedContact";
 
 function App() {
